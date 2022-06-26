77e39efa773744a84370fa1c803132e86ba550dc
BigLeno <rutileno.silva.702@ufrn.edu.br>
2022-06-26 12:37:21 -0300

    corrigido

--- Game of bird.py
+++ Game of bird.py
@@ -9,7 +9,7 @@ img_cano = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe
 img_chao = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
 img_fundo = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
 imgs_passaro =  [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png')))
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png')))
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
 ]
