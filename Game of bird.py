d34c25a834ac858e3adbee55abdef8e1a49a6913
BigLeno <rutileno.silva.702@ufrn.edu.br>
2022-06-26 12:36:13 -0300

    ínicio do game

--- Game of bird.py
+++ Game of bird.py
@@ -1,3 +1,15 @@
 import pygame
 import os
 import random
+
+tela_altura = 700
+tela_largura = 600
+
+img_cano = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+img_chao = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+img_fundo = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+imgs_passaro =  [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png')))
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png')))
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+]
